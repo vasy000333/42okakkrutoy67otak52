087dfaa076b0aa1618aca83f7e89e048781dad34
vasy000333 <greenboym9@gmail.com>
2026-08-31 11:54:48 +0300

    Update file2.js
    ooooooooooooooooooooooooo

--- file2.js
+++ file2.js
@@ -1,49 +1,50 @@
 {
-const bukvj = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    const bukvj = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
-const tldnum = Math.floor(Math.random() * 2) + 2;
+    const tldnum = Math.floor(Math.random() * 2) + 2;
 
-const tld1 = bukvj[Math.floor(Math.random() * bukvj.length)];
-const tld2 = bukvj[Math.floor(Math.random() * bukvj.length)];
-const tld3 = bukvj[Math.floor(Math.random() * bukvj.length)];
+    const tld1 = bukvj[Math.floor(Math.random() * bukvj.length)];
+    const tld2 = bukvj[Math.floor(Math.random() * bukvj.length)];
+    const tld3 = bukvj[Math.floor(Math.random() * bukvj.length)];
 
-let tld = "";
+    let tld = "";
 
-if (tldnum === 3) {
-    tld = tld1 + tld2 + tld3;
-} else {
-    tld = tld1 + tld2;
-}
+    if (tldnum === 3) {
+        tld = tld1 + tld2 + tld3;
+    } else {
+        tld = tld1 + tld2;
+    }
 
-tld = "." + tld;
+    tld = "." + tld;
 
-let dominnum = Math.floor(Math.random() * 63) + 1;
+    let dominnum = Math.floor(Math.random() * 63) + 1;
 
-const protokol1 = [
-    "https://",
-    "http://",
-    "ftp://",
-    "",
-    "sftp://"
-];
+    const protokol1 = [
+        "https://",
+        "http://",
+        "ftp://",
+        "",
+        "sftp://"
+    ];
 
-const protokol = protokol1[Math.floor(Math.random() * protokol1.length)];
+    const protokol = protokol1[Math.floor(Math.random() * protokol1.length)];
 
-let domin = "";
+    let domin = "";
 
-while (dominnum) {
-    const dominbuk = bukvj[Math.floor(Math.random() * bukvj.length)];
-    domin += dominbuk;
-    dominnum--;
-}
+    while (dominnum) {
+        const dominbuk = bukvj[Math.floor(Math.random() * bukvj.length)];
+        domin += dominbuk;
+        dominnum--;
+    }
 
-domin += tld;
-domin = protokol + domin;
+    domin += tld;
+    domin = protokol + domin;
 
-console.log(domin);
-const link = document.getElementById("domain333");
+    console.log(domin);
 
-link.href = domin;
-link.textContent = domin;
-link.style.color = "green";
+    const link = document.getElementById("domain333");
+
+    link.href = domin;
+    link.textContent = domin;
+    link.style.color = "green";
 }
