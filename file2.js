5e152c388ffb18261b8d67f453d9fe1acbd7fb4c
vasy000333 <greenboym9@gmail.com>
2026-08-31 11:25:08 +0300

    Update file2.js
    kkkkkk

--- file2.js
+++ file2.js
@@ -1,1 +1,46 @@
+function generateDomain() {
+    const bukvj = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
+    const tldnum = Math.floor(Math.random() * 2) + 2;
+
+    const tld1 = bukvj[Math.floor(Math.random() * bukvj.length)];
+    const tld2 = bukvj[Math.floor(Math.random() * bukvj.length)];
+    const tld3 = bukvj[Math.floor(Math.random() * bukvj.length)];
+
+    let tld = "";
+
+    if (tldnum === 3) {
+        tld = tld1 + tld2 + tld3;
+    } else {
+        tld = tld1 + tld2;
+    }
+
+    tld = "." + tld;
+
+    const dominnum = Math.floor(Math.random() * 63) + 1;
+
+    const protokol1 = [
+        "https://",
+        "http://",
+        "ftp://",
+        "",
+        "sftp://"
+    ];
+
+    const protokol = protokol1[Math.floor(Math.random() * protokol1.length)];
+
+    let domin = "";
+
+    for (let i = 0; i < dominnum; i++) {
+        const dominbuk = bukvj[Math.floor(Math.random() * bukvj.length)];
+        domin += dominbuk;
+    }
+
+    domin += tld;
+    domin = protokol + domin;
+    const link = document.getElementById("domain333");
+
+    link.href = domin;
+    link.textContent = domin;
+    link.style.color = "green";
+}
