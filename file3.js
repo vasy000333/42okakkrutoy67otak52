3ec6e17606406f3cc7759b48d2a7b0229ebd28af
vasy000333 <greenboym9@gmail.com>
2026-08-31 12:07:29 +0300

    Create file3.js
    file3

---
+++ file3.js
@@ -0,0 +1,15 @@
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+let id = "";
+
+for (let i = 0; i < 11; i++) {
+    id += chars[Math.floor(Math.random() * chars.length)];
+}
+
+const url = "https://www.youtube.com/watch?v=" + id;
+
+const link = document.getElementById("youtubeLink");
+
+link.href = url;
+link.textContent = url;
+link.style.color = "green";
